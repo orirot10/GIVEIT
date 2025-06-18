4b72414f33909345a8568fb1bb853940a89ebdd2
OriRoth22 <rori@post.bgu.ac.il>
2025-06-18 17:41:17 +0300

    Fix firebase-admin deployment

--- backend/routes/rentalRoutes.js
+++ backend/routes/rentalRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const { uploadNewRental, getRentals, getUserRentals, editRental, deleteRental, searchRentals, filterRentals } = require('../controllers/rentalController.js');
-const requireAuth = require('../middleware/authMiddleware.js');
+const { protect } = require('../middleware/authMiddleware.js');
 const upload = require('../middleware/upload');
 
 const router = express.Router();
 
 // Protected route
-router.post('/', requireAuth, upload.array('images', 5), uploadNewRental);
+router.post('/', protect, upload.array('images', 5), uploadNewRental);
 router.get('/', getRentals);
-router.get('/user', requireAuth, getUserRentals);
-router.put('/:id', requireAuth, editRental);
-router.delete('/:id', requireAuth, deleteRental);
+router.get('/user', protect, getUserRentals);
+router.put('/:id', protect, editRental);
+router.delete('/:id', protect, deleteRental);
 router.get("/search", searchRentals);
 router.get('/filter', filterRentals);
 
