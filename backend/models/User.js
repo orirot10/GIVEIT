7b6bfac1fb75ea4dd3ab0bf0fe838136b416ac6c
OriRoth22 <rori@post.bgu.ac.il>
2025-06-20 09:46:45 +0300

    Fix firebase edit user

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,15 +2,18 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firebaseUid: { type: String, required: true, unique: true },
-    firstName: { type: String, required: true },
-    lastName:  { type: String, required: true },
+    firstName: { type: String },
+    lastName:  { type: String },
     phone:     { type: String },
     email:     { type: String, required: true, unique: true },
     country:   { type: String },
     city:      { type: String },
     street:    { type: String },
     photoURL:  { type: String },
-    }, {
+    displayName: { type: String },
+    photoUrl: { type: String },
+    createdAt: { type: Date, default: Date.now },
+}, {
     timestamps: true,
 });
 
