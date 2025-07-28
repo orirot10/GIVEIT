9d9c34b4f4a6aa5894c36e5b5cae01e1f18840fe
orirot10 <orirot13@gmail.com>
2025-07-28 12:28:13 +0300

    Style: match toggle colors to listing status

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,9 @@
-module.exports = {
-    content: [
-    './src/**/*.{html,js,jsx,ts,tsx}', // ודא שאתה סורק את כל הקבצים של ה-React
-    ],
-    plugins: [
-    require('daisyui'), // הוסף את הפלאגין כאן
-    ],
-    daisyui: {
-    themes: ["light", "dark", "cupcake"], // תוכל להוסיף את הנושאים שברצונך להשתמש בהם
-    },
-}
+import daisyui from 'daisyui';
+
+export default {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ['light', 'dark', 'cupcake'],
+  },
+};
