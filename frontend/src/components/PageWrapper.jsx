8e9d3f3c12673af96b7c9ad59c773f517412e351
orirot10 <orirot13@gmail.com>
2025-07-31 18:58:18 +0300

    feat: add givit icon to page wrapper

--- frontend/src/components/PageWrapper.jsx
+++ frontend/src/components/PageWrapper.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import givitIcon from '../../resources/givit.svg';
 
 const PageWrapper = ({ children }) => {
   return (
     <div className="relative w-full min-h-screen">
+      <img
+        src={givitIcon}
+        alt="givit"
+        className="absolute top-2 left-2 w-8 h-8"
+      />
       <div className="w-full">
         {children}
       </div>
@@ -10,4 +16,4 @@ const PageWrapper = ({ children }) => {
   );
 };
 
-export default PageWrapper; 
+export default PageWrapper;
